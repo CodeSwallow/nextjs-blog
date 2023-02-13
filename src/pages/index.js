ab1166e008b93600a9f57b13297b07767c2e9eed
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-02-13 13:06:49 -0600

    initialized amplify

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div></div>
+        <div>Hello!</div>
       </main>
     </>
   )
